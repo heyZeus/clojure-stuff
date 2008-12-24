a1b481e23e3f8bdead01f467563c07ee0b9c6453
Brian Doyle <brianpdoyle@gmail.com>
2008-12-23 22:46:42 -0700

    no longer loads in the test files

--- repl-init.clj
+++ repl-init.clj
@@ -3,14 +3,14 @@
 
 (defn name-to-symbol [name] 
   "Converts the name to something that can be passed to the symbol function"
-  (.replaceAll (.replaceAll (.substring name 0 (- (.lastIndexOf name "clj") 1)) "_" "-") "/" "."))
+  (symbol (.replaceAll (.replaceAll (.substring name 0 (- (.lastIndexOf name "clj") 1)) "_" "-") "/" ".")))
 
 (defn contrib-ns [jar]
   "Returns a seq of symbols in the contrib libs in the clojure.contrib package"
   (for [f (map #(.getName %) 
                (enumeration-seq (.entries (java.util.zip.ZipFile. jar))))
-        :when (and (.endsWith f "clj") (= 3 (count (.split f "/"))))]
-    (symbol (name-to-symbol f))))
+        :when (and (.endsWith f "clj") (= 3 (count (.split f "/"))) (not (.contains f "test")))]
+    (name-to-symbol f)))
 
 ; sets the variable to the path to the colure-contrib.jar file, otherwise nil
 (def contrib-jar (if-let [url (.getResource (ClassLoader/getSystemClassLoader) "clojure-contrib.jar")]
