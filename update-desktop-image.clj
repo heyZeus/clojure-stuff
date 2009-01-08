ea802ffecac8b7983ed39a04a9cdf11c6c985d80
Brian Doyle <brianpdoyle@gmail.com>
2009-01-07 23:54:44 -0700

    added more function docs

--- update-desktop-image.clj
+++ update-desktop-image.clj
@@ -8,28 +8,37 @@
 
 (def image-name (str (.getProperty (System/getProperties) "user.home") "/desktop-image.jpg"))
 
-(defn potential-files [url resolution]
-    (for [name (map #(last (.split %1 " ")) 
-                 (.split (streams/slurp* url) "\n")) 
-        :when (.endsWith name (str "_" resolution ".jpg"))] name))
+(defn potential-files 
+  "Returns a seq of potential image filenames, all relative paths."
+  [url resolution]
+    (for [name (map #(last (.split %1 " ")) (.split (streams/slurp* url) "\n")) 
+            :when (.endsWith name (str "_" resolution ".jpg"))] name))
 
 (defn write-bytes 
   "Writes the bytes from the in-stream to the given filename."
-  [#^java.io.InputStream in-stream #^String filename]
-  (with-open [out-stream (new FileOutputStream filename)]
-    (let [buffer (make-array (Byte/TYPE) 4096)]
-      (loop [bytes (.read in-stream buffer)]
-        (if (not (neg? bytes))
-          (do 
-            (.write out-stream buffer 0 bytes)
-            (recur (.read in-stream buffer))))))))
+  ([#^java.io.InputStream in-stream #^String filename #^Integer buffer-size]
+    (with-open [out-stream (new FileOutputStream filename)]
+      (let [buffer (make-array (Byte/TYPE) buffer-size)]
+        (loop [bytes (.read in-stream buffer)]
+          (if (not (neg? bytes))
+            (do 
+              (.write out-stream buffer 0 bytes)
+              (recur (.read in-stream buffer))))))))
+  ([#^java.io.InputStream in-stream #^String filename]
+    (write-bytes in-stream filename 4096)))
 
-(defn select-file [ftp-str resolution]
+(defn select-file 
+  "Returns a random desktop image filename to download"
+  [resolution]
   (let [files (potential-files (URL. ftp-str) resolution)]
     (str ftp-str (nth files (rand-int (count files))))))
 
-(defn update [resolution]
+(defn update 
+  "Sets the desktop background to an image downloaded randomly from the internet."
+  [resolution]
    (let [tmp-image-name (str image-name ".tmp")]
+     ; downloading to a tmp file first because overwriting the existing
+     ; file causes some problems with gnome
      (with-open [r (.openStream (URL. (select-file ftp-str resolution)))]
        (write-bytes r tmp-image-name))
      (sout/sh "mv" "-f" tmp-image-name image-name)
