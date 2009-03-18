d392af83a85905d524004a8608a0a4f99cca78dc
Brian Doyle <bdoyle@localmatters.com>
2009-03-17 23:01:52 -0600

    spits out xml for the base business fields

--- zip2xml.clj
+++ zip2xml.clj
@@ -2,37 +2,47 @@
   (:import (java.util.zip ZipFile)
            (com.csvreader CsvReader)
            (java.nio.charset Charset))
-  (:require [clojure.contrib.duck-streams :as streams]))
+  (:require [clojure.contrib.duck-streams :as streams])
+  (:use [clojure.contrib.prxml]))
 
 (def records-per-file 10000)
 (def output-dir "/tmp/acxiom-clj2/")
-(def columns {:model_id 0
-              :name 1
-              :street 2
-              :city 8
-              :province 9
-              :postal-code 10
-              :phone 12
-              :fax 64
-              :website 32
-              :hours 66
-              :latitude 25
-              :longitude 26
-              :precision 27
-              :business-flg 23
-              :categories (range 48 53)})
+(def fields [[:name 1]
+             [:street 2]
+             [:city 8]
+             [:province_name "province_name"]
+             [:province 9]
+             [:postal-code 10]
+             [:phone 12]
+             [:fax 64]
+             [:website 32]
+             [:model "Opus::Business"]
+             [:latitude 25]
+             [:longitude 26]
+             [:precision 27]
+             [:refine "opus_business"]
+             [:model_id 0]
+             [:updated "NOW"]])
 
-(defn write-xml
-  [reader file-name]
-  (with-open [o (streams/writer file-name)]
-    (loop [line-cnt 0
-           has-next? true]
-      (if (and (< line-cnt records-per-file) has-next?)
-         (do
-           (.write o (.getRawRecord reader))
-           (.println o) 
-           (if (< (inc line-cnt) records-per-file)
-             (recur (inc line-cnt) (.readRecord reader))))))))
+(defn create-doc-xml
+  [reader]
+  (let [values (vec (.getValues reader))]
+    (if (not (empty? values))
+      (reduce (fn [ret [field k]] 
+                (if (string? k)
+                  (conj ret [field k])
+                  (conj ret [field (values k)])))
+              [:doc]
+              fields))))
+
+(defn create-file-xml
+  [reader] 
+  (doall (loop [line-cnt 0
+         xml [:add]
+         next-record? true]
+    (if (and (< line-cnt records-per-file) next-record?)
+      (recur (inc line-cnt) (conj xml (create-doc-xml reader)) (.readRecord reader))
+      xml))))
  
 (defn process-zip-file 
   [file]
@@ -42,13 +52,15 @@
         (with-open [reader (CsvReader. (.getInputStream zfile f) \, (Charset/forName "US-ASCII"))]
           (loop [filenum 0]
             (let [o-file (format "%s-%04d.xml" basename filenum)]
-               (write-xml reader o-file) 
-               (if (.readRecord reader)
-                 (recur (inc filenum))))))))))
+               (with-open [o (streams/writer o-file)]
+                   (binding [*out* o] 
+                     (prxml [:decl! {:version 1.1}] (create-file-xml reader)))
+                   (if (.readRecord reader)
+                     (recur (inc filenum)))))))))))
 
 (defn main 
   []
-  (println (time (pmap #(process-zip-file %1) ["/home/bdoyle/tmp/acxiom_oct/busreg1.zip"]))))
+  (println (time (dorun (pmap #(process-zip-file %1) ["/home/bdoyle/tmp/acxiom_oct/busreg1.zip"])))))
 
 ; process all of the acxiom files
 (main)
