235d132ac23d1007ad88c933f5f4c8df3c64350d
Brian Doyle <brianpdoyle@gmail.com>
2009-10-31 17:30:51 -0600

    updated the documentation

--- update-desktop-image.clj
+++ update-desktop-image.clj
@@ -7,7 +7,7 @@
 
 (def ftp-str (str "ftp://anonymous: @ftp.gnome.org" "/Public/GNOME/teams/art.gnome.org/backgrounds/"))
 
-(defn potential-files 
+(defn potential-images
   "Returns a seq of potential image filenames, all relative paths."
   [url resolution]
     ; the output from the streams/slurp will be something like: 
@@ -16,14 +16,15 @@
     ; -rw-r--r--    1 ftp      ftp        766046 Aug 27  2006 OTHER-WhiteQueenKilledTheKing_1400x1050.jpg
     ; -rw-r--r--    1 ftp      ftp       1265120 Aug 27  2006 OTHER-WhiteQueenKilledTheKing_1920x1200.jpg
     ; need to split on the space and grab the last field to get the filename, then check to see if 
-    ; the filename has the correct screen resolution
+    ; the filename has the correct screen resolution in the name
     (for [name (map #(last (.split %1 " ")) (.split (streams/slurp* url) "\n")) 
             :when (.endsWith name (str "_" resolution ".jpg"))] name))
 
-(defn select-new-background
-  "Returns a random desktop filename to download"
+(defn select-new-image
+  "Returns a random desktop filename to download, this is the full path to the file."
   [resolution]
-  (let [files (potential-files (URL. ftp-str) resolution)]
+  ; get the potential files and return the full file path
+  (let [files (potential-images (URL. ftp-str) resolution)]
     (str ftp-str (nth files (rand-int (count files))))))
 
 (defn update-image 
@@ -32,8 +33,8 @@
    (let [tmp-image-name (str image-name ".tmp")]
      ; downloading to a tmp file first because overwriting the existing
      ; file causes some problems with gnome
-     (with-open [r (.openStream (URL. (select-new-background resolution)))]
-       (streams/copy r (File. tmp-image-name)))
+     (with-open [i (.openStream (URL. (select-new-image resolution)))]
+       (streams/copy i (File. tmp-image-name)))
      (sout/sh "mv" "-f" tmp-image-name image-name)
      ; updates the background image
      (sout/sh "/usr/bin/gconftool" "-s" 
@@ -44,7 +45,7 @@
   (or (first *command-line-args*) "1680x1050"))
 
 (comment " 
-  Call this script from the command line and only takes one argument, your screen resolution.
+  Call this script from the command line and it only takes one argument, your screen resolution.
   This assumes you are running gnome and have access to the internet.
 
   clj update-desktop-image 1024x178
